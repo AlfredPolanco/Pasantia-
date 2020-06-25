4bb39d9540edad3bb3093d37e644c0ff9dbb792e
AlfredPolanco <39679348+AlfredPolanco@users.noreply.github.com>
2020-06-25 11:23:41 -0400

    Update BoletinData.py

--- Python/BoletinData.py
+++ Python/BoletinData.py
@@ -1,5 +1,15 @@
 from tika import parser 
 raw = parser.from_file('Boletin-especial-96.pdf')
 print(raw['content'])
-
 #pip install tika
+
+
+##pip install tabula-py
+## este es el que da mucho NaN
+import tabula
+# readinf the PDF file that contain Table Data
+# you can find find the pdf file with complete code in below
+# read_pdf will save the pdf table into Pandas Dataframe
+df = tabula.read_pdf("Boletin-especial-96.pdf")
+# in order to print first 5 lines of Table
+print(df)
